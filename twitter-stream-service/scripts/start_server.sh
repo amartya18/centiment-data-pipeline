229d1119f79047db3eac847d38e3607221b2db78
amartya18 <amartya.sk@gmail.com>
2022-06-12 18:41:12 +0700

    update start_server.sh

--- twitter-stream-service/scripts/start_server.sh
+++ twitter-stream-service/scripts/start_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/pm2 start /home/ec2-user/twitter-stream-service/main.py --name twitter-stream-service --interpreter ~/twitter-stream-service/environment/bin/python
+/usr/bin/pm2 start /home/ec2-user/twitter-stream-service/main.py --name twitter-stream-service --interpreter /home/ec2-user/twitter-stream-service/environment/bin/python
