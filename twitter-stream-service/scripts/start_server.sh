ac94ab682cee9bd9833aac8d57f9920798f1c2f2
amartya18 <amartya.sk@gmail.com>
2022-06-12 14:22:33 +0700

    add temporary start_server.sh

--- twitter-stream-service/scripts/start_server.sh
+++ twitter-stream-service/scripts/start_server.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+pwd
+~/.nvm/versions/node/v16.15.1/bin/pm2 status
 ~/.nvm/versions/node/v16.15.1/bin/pm2 start /home/ec2-user/twitter-stream-service/main.py --name twitter-stream-service
