7669490f4f88a1702544f74156453a9760e44cb1
amartya18 <amartya.sk@gmail.com>
2022-06-12 18:05:55 +0700

    update pm2 start with proper python venv interpreter

--- twitter-stream-service/scripts/start_server.sh
+++ twitter-stream-service/scripts/start_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/pm2 start /home/ec2-user/twitter-stream-service/main.py --name twitter-stream-service
+/usr/bin/pm2 start /home/ec2-user/twitter-stream-service/main.py --name twitter-stream-service --interpreter ~/twitter-stream-service/environment/bin/python
