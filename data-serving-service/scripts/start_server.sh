0c393ae88e3666c83b92f6b207e036ea8628f6e9
amartya18 <amartya.sk@gmail.com>
2022-07-01 18:55:41 +0700

    data-serving-service: use gunicorn in start_server

--- data-serving-service/scripts/start_server.sh
+++ data-serving-service/scripts/start_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/pm2 start "/home/ec2-user/data-serving-service/environment/bin/uvicorn main:app --host 0.0.0.0 --port 5001" --name data-serving-service --interpreter /home/ec2-user/data-serving-service/environment/bin/python --no-autorestart
+/usr/bin/pm2 start "/home/ec2-user/data-serving-service/environment/bin/gunicorn main:app --workers 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:5001" --name data-serving-service
