96c8a8be3a92db5838f8f77c5e9b83aeb84a98a6
amartya18 <amartya.sk@gmail.com>
2022-07-01 19:12:43 +0700

    data-serving-service: start_server cd to project folder

--- data-serving-service/scripts/start_server.sh
+++ data-serving-service/scripts/start_server.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-/usr/bin/pm2 start "/home/ec2-user/data-serving-service/environment/bin/gunicorn main:app --workers 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:5001" --name data-serving-service
+cd data-serving-service/
+/usr/bin/pm2 start "/home/ec2-user/data-serving-service/environment/bin/gunicorn main:app --workers 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:5001" --name data-serving-service --no-autorestart
